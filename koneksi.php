b6e091aed98bb04349ce252760fa2dd9a445da9d
KINGBOTs1 <ds767365@gmail.com>
2026-05-31 14:13:12 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$koneksi = mysqli_connect("localhost", "root", "", "uts_perpustakaan");
+$koneksi = mysqli_connect("database-perpustakaan-q6gl4b", "perpustakaan", "123perpustakaan", "perpustakaan", 3306);
 
 if(!$koneksi){
     die("Koneksi gagal: ".mysqli_connect_error());
